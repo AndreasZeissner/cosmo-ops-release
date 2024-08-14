88feca0eb43e40a9204d53431225f4f51da72093
Andreas Zeissner <andi@wundergraph.com>
2024-08-14 22:23:26 +0200

    chore: change bg color

--- pages/sentry-example-page.jsx
+++ pages/sentry-example-page.jsx
@@ -11,6 +11,7 @@ export default function Page() {
 
       <main
         style={{
+          backgroundColor: "red",
           minHeight: "100vh",
           display: "flex",
           flexDirection: "column",
